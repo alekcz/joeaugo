95c784160af3f8ff7b92f6996b3842789b14a587
Alex Oloo <alekcz@gmail.com>
2021-07-18 19:33:06 +0200

    add placeholder

--- public/index.clj
+++ public/index.clj
@@ -1,15 +1,21 @@
 (ns index
   (:require [pcp :as pcp]
-            [api.info :as a]
             [garden.core :refer [css]]))
 
 (def resp 
   (pcp/html
-    [:html {:style "font-family: 'Source Sans Pro', Arial, Helvetica, sans-serif;text-align: center;"
+    [:html {:style "font-family: 'Thasadith', sans-serif;text-align: center;"
             :lang "en"}
       [:head 
-        [:title "PCP website"]
+        [:title "Joe Odhiambo Augo - In Memoriam"]
+        [:meta {:charset "utf-8"}]
+        [:meta {:name "viewport" :content "width=device-width,initial-scale=1"}]
+        [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge"}]
+        [:link {:rel "preconnect" :href "https://fonts.googleapis.com"}]
+        [:link {:rel "preconnect" :href "https://fonts.gstatic.com" :crossorigin nil}]
+        [:link {:href "https://fonts.googleapis.com/css2?family=Thasadith&display=swap" :rel "stylesheet"}]
         [:style 
+          (css [:body {:background-color "#fffff4"}])
           (css [:p.info { :background-color "#EEE" 
                           :font-size "13px"
                           :margin-top "40px"
@@ -18,28 +24,17 @@
                           :width "300px"}])
           (css [:strong { :font-size "13px"}])
           (css [:code   { :font-size "12px" 
-                          :font-weight "normal"}])]]
+                          :font-weight "normal"}])
+          (css [:.profile {:border-radius "100%"}])
+          (css [:h1 {:font-size "60px" :margin-top "0px"}])
+          (css [:h2{:font-size "30px" :margin-bottom "0px"}])]]
       [:body 
         [:div 
               {:style "display: flex; flex-direction: column; justify-content: center; align-items: center; min-height: 90vh;"
-              :ondblclick "alert('Now we're cooking with gas')"}
-              [:img {:src "https://raw.githubusercontent.com/alekcz/pcp/master/assets/logo/logo-alt.svg" :width "200px"}]
-              [:h1 "Your PCP site is up and running."]
-              [:p "Your json endpoint is here " [:a {:href "/api/info.clj"} "here"] 
-                [:br]
-                "Learn more about pcp at " [:a {:href a/repo :target "_blank"} "here"]
-                [:br]
-                "Happy scripting!"]
-              [:p.info
-                [:strong "Request uri "] [:span (str (-> pcp/request :request-uri))]
-                [:br] 
-                [:strong "Request method "] [:span (str (-> pcp/request :request-method))]
-                [:br] 
-                [:strong "Request scheme "] [:span (str (-> pcp/request :request-scheme))]
-                [:br] 
-                [:strong "User agent "]
-                [:br] 
-                [:code (str (-> pcp/request :http-user-agent))]
-                [:br]]]]]))
+              :data-null ""}
+              [:img.profile {:src "/profile.jpeg" :width "200px"}]
+              [:br][:br]
+              [:h2 "In loving memory of"]
+              [:h1 "Joe Odhiambo Augo"]]]]))
 
 (pcp/response 200 resp "text/html")            
